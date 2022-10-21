6f8dbe2ad1eef701a233e6041c58871b184408a1
Pablo <could_2032@outlook.com>
2022-10-21 10:38:49 -0300

    modificando visualización

---
+++ Menu.Desp_js/visual.js
@@ -0,0 +1,13 @@
+const miniImg = document.querySelectorAll(".box_img");
+const imgZoom = document.querySelector(".box_img__zoom");
+const contImagen = document.querySelector(".img_cont");
+
+contImagen.addEventListener("click" , (e)=>{
+
+ for(let content in contImagen){
+   
+   return console.log(miniImg.valueOf());
+ }
+ 
+})
+
