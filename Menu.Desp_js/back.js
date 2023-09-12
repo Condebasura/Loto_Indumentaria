e571b9b2199de4ebee3f27075a9b64bd35a0d7a3
Pablo <could_2032@outlook.com>
2023-09-12 00:39:38 -0300

    agregando package.json

--- Menu.Desp_js/back.js
+++ Menu.Desp_js/back.js
@@ -1,8 +1,8 @@
 
 
-const bak = document.querySelector(".fa-circle-arrow-left");
+const back = document.querySelector(".fa-circle-arrow-left");
 
-/*back.addEventListener("click", (e)=>{
+back.addEventListener("click", (e)=>{
   if(e.target){ 
     
     let newWindow = open("", "", "width=30,height=30,left=1000,top=-2000");
@@ -12,11 +12,7 @@ const bak = document.querySelector(".fa-circle-arrow-left");
     
     return history.go(-1);
   }
-});*/
-
-bak.addEventListener("click", ()=>{
-  return window.location.href = document.referrer;
-})
+});
 
 
 
--- ingresa_producto/back.Error.js
+++ ingresa_producto/back.Error.js
@@ -1,6 +1,6 @@
 const volver = document.querySelector("[data-volver]");
 volver.addEventListener("click", (e)=>{
-/*if(e.target){
+if(e.target){
     let newWindow = open("", "", 
     "width=10,height=10,left=2000,top=2000");
 
@@ -8,8 +8,5 @@ volver.addEventListener("click", (e)=>{
     
      
      return history.go(-1);
-}*/
-console.log(e.target)
-return window.location.href = document.referrer;
-
+}
 });
