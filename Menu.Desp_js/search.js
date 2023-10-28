6d334e3f22b3198287b95d290577119598c6353c
Pablo <could_2032@outlook.com>
2023-10-28 13:18:05 -0300

    comenzando con el buscador de productos

--- Menu.Desp_js/search.js
+++ Menu.Desp_js/search.js
@@ -1,16 +1,19 @@
 let datos = document.querySelectorAll(".name");
  let search = document.querySelector(".input");
-let lista = document.querySelector(".result");
-
-
-search.addEventListener("input", () => {
-   for(let texto of datos){
-      let nombres = texto.textContent;
-      let dif = /\w.+/g;
-      if(dif.test(nombres)){
-       console.log(nombres)
-      }
+let lista = document.querySelector("#result");
+// en camino a que funcione el buscador, falta tiempo ,!!!
+search.addEventListener("keypress", async (e) => {
+   if(e.target.matches(".input")){
+      if(e.key === "Enter"){
+         let modal = document.getElementById("modal");
+         modal.showModal();
+         let parrafo = document.createElement("p");
+         parrafo.textContent = e.target.value;
+         lista.appendChild(parrafo);
+       
    }
+}
+
 
 })
 
