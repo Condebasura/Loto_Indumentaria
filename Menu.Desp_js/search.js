ea59cc1f231e6310b1b745d1fbdf4cb6b2c0ae12
Pablo <could_2032@outlook.com>
2023-10-29 09:46:31 -0300

    intentando un buscador interno

--- Menu.Desp_js/search.js
+++ Menu.Desp_js/search.js
@@ -1,18 +1,60 @@
 let datos = document.querySelectorAll(".name");
- let search = document.querySelector(".input");
+ const search = document.querySelector(".input");
 let lista = document.querySelector("#result");
+const modal = document.getElementById("modal");
+const urls =[
+   'Hom_Remeras',
+   'Hom_Pantalones',
+   'Hom_Accesorios',
+   'Wom_Remeras',
+   'Wom_Pantalones',
+   'Wom_Vestidos',
+   'Wom_Accesorios',
+   'Nena_Remeras',
+   'Nena_Pantalones',
+   'Nena_Vestidos',
+   'Nene_Remeras',
+   'Nene_Pantalones'
+];
 // en camino a que funcione el buscador, falta tiempo ,!!!
 search.addEventListener("keypress", async (e) => {
-   if(e.target.matches(".input")){
-      if(e.key === "Enter"){
-         let modal = document.getElementById("modal");
-         modal.showModal();
-         let parrafo = document.createElement("p");
-         parrafo.textContent = e.target.value;
-         lista.appendChild(parrafo);
-       
+   if(e.key === "Enter"){   
+    
+   for(let url of urls){
+      let prod = `http://localhost:3000/${url}`;
+      fetch(prod).then(res => res.json())
+      .then(data =>{
+         let datos = data;
+         console.log(datos)
+      })
+        
+   
+     
    }
-}
+    
+      
+      
+         
+         let salir = document.createElement("button");
+         salir.textContent = "salir";
+         
+       salir.addEventListener("click", e =>{
+         if(e.target){
+            modal.close();
+            lista.innerHTML = "";
+            search.value = "";
+         }
+       });
+       window.addEventListener("keyup", e =>{
+         if(e.key === "Escape") {
+            modal.close();
+            lista.innerHTML = "";
+            search.value = "";
+         }
+       });
+   }
+  
+
 
 
 })
