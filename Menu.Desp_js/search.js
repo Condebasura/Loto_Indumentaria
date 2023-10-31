00ddeb17bf335fc2fcee6fdc47e80a0ebda0edc5
Pablo <could_2032@outlook.com>
2023-10-30 21:02:43 -0300

    continuando con el buscador interno, debo modificargit add .

--- Menu.Desp_js/search.js
+++ Menu.Desp_js/search.js
@@ -1,7 +1,9 @@
-let datos = document.querySelectorAll(".name");
+
  const search = document.querySelector(".input");
-let lista = document.querySelector("#result");
+ const cajaProd = document.querySelector(".caja-prod");
+const lista = document.getElementById("result").content;
 const modal = document.getElementById("modal");
+const fragment = document.createDocumentFragment(); 
 const urls =[
    'Hom_Remeras',
    'Hom_Pantalones',
@@ -18,24 +20,41 @@ const urls =[
 ];
 // en camino a que funcione el buscador, falta tiempo ,!!!
 search.addEventListener("keypress", async (e) => {
-   if(e.key === "Enter"){   
-    
-   for(let url of urls){
-      let prod = `http://localhost:3000/${url}`;
-      fetch(prod).then(res => res.json())
-      .then(data =>{
-         let datos = data;
-         datos.forEach(el => {
-         if(el.producto.toLowerCase().includes(search.value.toLowerCase())){
-            console.log(el.producto, el.precio);
-         }
-         });
-      })
-        
-   
+   if(e.key === "Enter"){  
      
-   }
-    
+     
+      for(let url of urls){
+         let prod = `http://localhost:3000/${url}`;
+         fetch(prod).then(res => res.json())
+         .then(data =>{
+            let datos = data;
+            datos.forEach(el => {
+            if(el.producto.toLowerCase().includes(search.value.toLowerCase())){
+               lista.querySelector("h3").textContent = el.producto;
+               lista.querySelector("img").src = el.archivo;
+             
+
+           
+            let clone = document.importNode(lista,true);
+           fragment.appendChild(clone);
+          
+           modal.appendChild(fragment);
+        ;
+           
+           
+         
+           console.log(fragment)
+         }
+      });
+   })
+   modal.showModal();
+   
+}
+
+   
+
+   
+   
       
       
          
@@ -45,14 +64,14 @@ search.addEventListener("keypress", async (e) => {
        salir.addEventListener("click", e =>{
          if(e.target){
             modal.close();
-            lista.innerHTML = "";
+            modal.innerHTML = "";
             search.value = "";
          }
        });
        window.addEventListener("keyup", e =>{
          if(e.key === "Escape") {
             modal.close();
-            lista.innerHTML = "";
+            modal.innerHTML = "";
             search.value = "";
          }
        });
