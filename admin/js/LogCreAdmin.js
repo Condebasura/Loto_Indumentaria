aa330af48cfe373272c302da063e8c46ceaa8d03
Pablo Zabala <could_2032@outlook.com>
2024-10-09 00:03:29 -0300

    Creando interfaz de ingreso y creacion para administradores

--- admin/js/LogCreAdmin.js
+++ admin/js/LogCreAdmin.js
@@ -37,8 +37,45 @@ document.addEventListener("DOMContentLoaded", (e)=>{
     Create.addEventListener("click",(e)=>{
         e.preventDefault();
         if(e.target){
+            modal.close();
+            modal.innerHTML = "";
           const form2 = document.createElement("form")
           const titulo2 = document.createElement("span");
+          const LabelUser2 = document.createElement("label");
+        const InputUser2 = document.createElement("input");
+       const LabelPass2 = document.createElement("label");
+        const InputPass2 = document.createElement("input");
+        const btn2 = document.createElement("button");
+        const Login = document.createElement("a");
+
+        InputPass2.setAttribute("type", "password")
+        btn2.innerHTML = "Enviar";
+        Login.innerHTML = "Login";
+        titulo2.innerHTML = "Create";
+        LabelUser2.innerHTML = "Usuario";
+        LabelPass2.innerHTML = "Pasword";
+        Login.style.cursor = "pointer";
+        cuerpo.style.backgroundColor =  "rgba(50,40,20,0.111)"
+       modal.style.backgroundColor = " rgba(13,13, 42, 0.66)";
+       form2.style.display = "flex";
+       form2.style.flexDirection = "column";
+       form2.style.gap =  ".5em";
+         Login.style.fontSize = "10px";
+         form2.appendChild(titulo2)
+        form2.appendChild(LabelUser2)
+        form2.appendChild(InputUser2)
+        form2.appendChild(LabelPass2)
+        form2.appendChild(InputPass2)
+        form2.appendChild(btn2);
+        form2.appendChild(Login)
+        modal.appendChild(form2)
+        modal.showModal();
+        Login.addEventListener("click", (e)=>{
+            e.preventDefault();
+            if(e.target){
+                return location.reload();
+            }
+        })
         }
     })
     }
