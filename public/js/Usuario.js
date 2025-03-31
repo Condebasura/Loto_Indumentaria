42ca3ad8847d3f1f57fb92146b486897e7f8e11b
Pablo Zabala <could_2032@outlook.com>
2025-03-31 11:04:34 -0300

    continuando con favoritos

--- public/js/Usuario.js
+++ public/js/Usuario.js
@@ -7,7 +7,55 @@ const cajaUltimas = document.querySelector(".cajaUltimas");
 const contUltimas = document.querySelector(".contUltimas");
 const prodSearch = document.querySelector(".product");
 
-
+const funcModal = (textBody, titulo)=>{
+    modalcontainer.innerHTML = "";
+    
+     const modtabi = document.createElement("div");
+     const modDialog = document.createElement("div");
+     const modContent = document.createElement("div")
+     const modHeader = document.createElement("div");
+    
+     const btnClose = document.createElement("button");
+     const modBody = document.createElement("div");
+     
+    
+     modtabi.setAttribute("class","modal");
+     modtabi.setAttribute("tabindex","-1");
+     modDialog.setAttribute("class","modal-dialog");
+     modContent.setAttribute("class","modal-content");
+     modHeader.setAttribute("class","modal-header text-bg-primary");
+     titulo.setAttribute("class","modal-title");
+     btnClose.setAttribute("class","btn-close");
+     btnClose.setAttribute("type","button");
+     btnClose.setAttribute("data-bs-dismiss","modal");
+     btnClose.setAttribute("aria-label","Close");
+     textBody.setAttribute("class", "row p-5 justify-content-center");
+    
+     modtabi.appendChild(modDialog);
+     modDialog.appendChild(modContent);
+     modContent.appendChild(modHeader);
+     modContent.appendChild(modBody);
+  
+     modHeader.appendChild(titulo);
+     modHeader.appendChild(btnClose);
+  
+     modBody.appendChild(textBody);
+   
+      
+   modalcontainer.innerHTML = "";
+  modalcontainer.appendChild(modtabi);
+  
+  modtabi.removeAttribute("inert");
+  modtabi.removeAttribute("aria-hidden");
+      const bootstrapModal = new bootstrap.Modal(modtabi);
+              bootstrapModal.show();
+         
+      modtabi.addEventListener("hidden.bs.modal", ()=>{
+        modalcontainer.innerHTML = "";
+        modtabi.setAttribute("aria-hidden", "true");
+        modtabi.setAttribute("inert", "")
+      })
+    }
 
 const verProd = async (el, bestPrecio, rebajadoDe, imagenObjectURL, interes) => {
 
@@ -845,7 +893,7 @@ const dataUsuario = async () => {
                     fav.setAttribute("class", "list-group-item fav");
                     fav.innerHTML = el.producto;
                     ulFav.appendChild(fav);
-                    console.log(el);
+                    
                     let bestPrecio = Number(el.precio);
                     let desc = Number(el.descuento);
                     let porcentaje = (bestPrecio * desc) / 100;
