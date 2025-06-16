70ca3dd60402b7d66e0ea1bb42918c32bfcbb0fa
Pablo Zabala <could_2032@outlook.com>
2025-06-16 02:08:45 -0300

    Arreglando error en app.js (maxAge estaba mal ubicado)

--- app.js
+++ app.js
@@ -148,9 +148,9 @@ app.use(express.static(path.join(__dirname, 'admin')));
 app.use( 'uploads/',express.static(path.join(__dirname, "public/uploads/"),
 {maxAge: '24h'}
 ));
-app.use( 'img/',express.static(path.join(__dirname, "public/img/", {
-  maxAge: '24h'
-})));
+app.use( 'img/',express.static(path.join(__dirname, "public/img/"),
+{maxAge: '24h'}
+));
 
 
 app.use(express.json());
